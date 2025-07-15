19ce96305a68b8b3f78ccd5591a8a1de4a2f2ce1
Yunu Cho <yunu121@gmail.com>
2025-07-16 09:40:57 +1200

    Remove commented out code

--- hut-finder-client/src/components/huts/HutImage.tsx
+++ hut-finder-client/src/components/huts/HutImage.tsx
@@ -2,25 +2,6 @@ import React from 'react'
 import { TentTree } from 'lucide-react'
 
 const HutImage = (hut: Hut): React.ReactNode => {
-  // if (
-  //   hut.imageUrl === null ||
-  //   hut.imageUrl === '' ||
-  //   hut.imageUrl.includes('no-photo')
-  // ) {
-  //   return (
-  //     <div className="flex w-full h-full justify-center items-center bg-gray-200 rounded-lg">
-  //       <TentTree className="w-1/4 h-1/4"/>
-  //     </div>
-  //   )
-  // }
-  // return (
-  //   <img
-  //     src={hut.imageUrl}
-  //     alt={hut.name}
-  //     loading="lazy"
-  //     className="rounded-lg"
-  //   />
-  // )
   return (
     <div className="relative w-full h-full">
       {hut.imageUrl && !hut.imageUrl.includes('no-photo') ? (
